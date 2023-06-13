59de97336f0c13421ba1e3398b217b84b65e9c43
Ronan Reinhardt <sakiscripts@proton.me>
2023-06-13 16:01:43 -0700

    added weather

--- script.js
+++ script.js
@@ -14,25 +14,26 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
 
 // Replace 'YOUR_API_KEY' with your actual API key
 const apiKey = "feaa29c2e60f14e6ef146030814d162a";
-const weatherLocation = 'San Jose, US';
+const weatherLocation = 'Cupertino, US';
 
 const url = `https://api.openweathermap.org/data/2.5/weather?q=${weatherLocation}&appid=${apiKey}`;
-/*
+
 fetch(url)
     .then(response => response.json())
     .then(data => {
     // Extract the relevant weather information from the 'data' object
-    const temperature = data.main.temp;
+    const temperatureKelvin = data.main.temp;
+    const temperatureCelsius = temperatureKelvin - 273.15; // Convert to Celsius
     const description = data.weather[0].description;
 
     // Format the weather information
-    const formattedWeather = `Currently ${temperature.toFixed(1)} &#8451; (${description}) in ${weatherLocation}`;
+    const formattedWeather = `Currently ${temperatureCelsius.toFixed(1)} &#8451; (${description}) in ${weatherLocation}`;
 
     // Update the HTML element with the formatted weather information
     const weatherDiv = document.getElementById('weather');
     weatherDiv.innerHTML = formattedWeather;
+    weatherDiv.style.color = 'white';
     })
     .catch(error => {
     console.log('Error:', error);
     });
-*/
